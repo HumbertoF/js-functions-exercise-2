f6fefe64d1a3bcecbeac8af65e0ee0eceef5cb22
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-02-14 21:41:50 -0500

    Init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+(function () {
+    /**
+     * takes a distance parameter. 
+     * Move the character forward (based on the 
+     * direction they are facing) the specified distance.
+     */
+    // moveForward(distance)
+
+    /**
+     * takes a distance parameter. Move the character backward 
+     * (based on the direction they are facing) the specified distance.
+     */
+    // moveBackward(distance)
+
+    /**
+     *  reverse the direction the character is facing.
+     */
+    // turnAround()
+
+    /**
+     * logs the current position to the console
+     */
+    // printLocation()
+
+    // // Uncomment these after your functions are written
+    // moveForward(5)
+    // moveBackward(3)
+    // printLocation() // should print (2)
+    // turnAround()
+    // moveForward(10)
+    // moveBackward(5)
+    // printLocation() // should print -3
+})();
